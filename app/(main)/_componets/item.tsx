cb723ca96172e0d558d5c9139d79f6884a126445
bruhGrassi <brunagm88@gmail.com>
2024-01-31 21:29:53 -0300

    feat: add skeleton, started documents list's component

--- app/(main)/_componets/item.tsx
+++ app/(main)/_componets/item.tsx
@@ -10,6 +10,7 @@ import {
   } from "lucide-react";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface ItemProps {
     id?: Id<"documents">;
@@ -78,6 +79,20 @@ export const Item = ({
                     <span className="text-xs">⌘</span>K
                 </kbd>
             )}
+        </div>
+    )
+}
+
+Item.Skeleton = function ItemSkeleton({level} : {level?: number}){
+    return (
+        <div
+          style={{
+            paddingLeft: level ? `${(level * 12) + 25}px` : "12px"
+          }}
+          className="flex gap-x-2 py-[3px]"
+        >
+            <Skeleton className="h-4 w-4" />
+            <Skeleton className="h-4 w-[30%]"/>
         </div>
     )
 }
