2487dfb1c7f883ec52795653b99fbfbcbc0eab21
bruhGrassi <brunagm88@gmail.com>
2024-02-14 10:16:27 -0300

    feat: creating document list subitens

--- app/(main)/_componets/document-list.tsx
+++ app/(main)/_componets/document-list.tsx
@@ -1,12 +1,14 @@
 "use client"
 
+import { Doc, Id } from "@/convex/_generated/dataModel";
 import { api } from "@/convex/_generated/api";
-import { Doc, Id} from "@/convex/_generated/dataModel";
 import { Item } from "./item";
 import { useQuery } from "convex/react";
-import { useParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { FileIcon } from "lucide-react";
+
 
 interface DocumentListProps {
     parentDocumentId?: Id<"documents">;
@@ -33,31 +35,62 @@ export const DocumentList = ({
         parentDocument: parentDocumentId
     });
 
+    console.log('documents', documents);
+
     const onRedirect = (documentId: string) => {
         router.push(`/documents/${documentId}`)
-    }
+    };
 
     //documents is only undefined when is loading
     if( documents === undefined){
         return (
             <>
-            <Item.Skeleton level={level} />
-            {level === 0 && (
-                <>
-                    <Item.Skeleton level={level} />
-                    <Item.Skeleton level={level} />
-                </>
-            )}
+                <Item.Skeleton level={level} />
+                {level === 0 && (
+                    <>
+                        <Item.Skeleton level={level} />
+                        <Item.Skeleton level={level} />
+                    </>
+                )}
             </>
         )
     }
 
     return (
         <>
-            <p 
-                style={{ paddingLeft: level ? `${(level * 12) + 25}px` : undefined }}>
+            <p
+                style={{
+                paddingLeft: level ? `${(level * 12) + 46}px` : undefined
+                }}
+                className={cn(
+                "hidden text-sm font-medium text-muted-foreground/80 pb-1",
+                expanded && "last:block",
+                level === 0 && "hidden"
+                )}
+            >
                 No pages inside
             </p>
+            {documents.map((document) => (
+                <div key={document._id}>
+                    <Item
+                        id={document._id}
+                        onClick={() => onRedirect(document._id)}
+                        label={document.title}
+                        icon={FileIcon}
+                        documentIcon={document.icon}
+                        active={params.documentId === document._id}
+                        level={level}
+                        onExpand={() => onExpand(document._id)}
+                        expanded={expanded[document._id]}
+                    />
+                    {expanded[document._id] && (
+                        <DocumentList
+                        parentDocumentId={document._id}
+                        level={level + 1}
+                        />
+                    )}
+                </div>
+            ))}
         </>
     )
 }
--- app/(main)/_componets/navigation.tsx
+++ app/(main)/_componets/navigation.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { cn } from "@/lib/utils";
-import { ChevronLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react"
+import { ChevronLeft, MenuIcon, Plus, PlusCircle, Search, Settings } from "lucide-react"
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
